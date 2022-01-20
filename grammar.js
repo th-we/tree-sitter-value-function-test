b2e68736efbf3f15137210a5da21eaa85b47270d
Thomas Weber <thomas.weber@notengrafik.com>
2022-01-20 08:47:59 +0100

    Attempt at fixing error with prec.dynamic

--- grammar.js
+++ grammar.js
@@ -5,6 +5,8 @@ module.exports = grammar({
     /\s/
   ],
 
+  conflicts: $ => [[$._quoted_function, $.value]],
+
   rules: {
     data: $ => seq(
       repeat(choice($.value, $._quoted_function))
@@ -17,15 +19,18 @@ module.exports = grammar({
 
     // "value" must include quotes because there can be empty values which would
     // otherwise disappear from the tree
-    value: $ => seq(
+    value: $ => prec.dynamic(0, seq(
       token('"'),
       optional($._value_content),
       token.immediate('"')
-    ),
+    )),
 
     // "function" must not contain quotes because it is the insertion point for
     // another grammar
-    _quoted_function: $ => seq(token('"'), $.function, token.immediate('"')),
+    _quoted_function: $ => prec.dynamic(1, seq(
+      token('"'), $.function, token.immediate('"')
+    )),
+
     // The host application treats all values starting with "(" and containing
     // "{" as a function
     function: $ => seq(
